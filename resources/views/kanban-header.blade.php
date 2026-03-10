af7fc485129e9371d8749ebb3bd8f079ac8eef5d
Wannes Matthys <wannesmatthys@gmail.com>
2026-03-10 19:57:57 +0100

    Implement lazy loading

--- resources/views/kanban-header.blade.php
+++ resources/views/kanban-header.blade.php
@@ -18,13 +18,13 @@
                     '--c-400': 'var(--success-400)', 
                     '--c-600': 'var(--success-600)'
                 }">
-                <span x-text="status.records.length"></span>
+                <span x-text="status.count !== undefined ? status.count : status.records.length"></span>
             </span>
         </div>
 
         <template x-if="status.totalAttribute">
             <span class="block text-sm font-normal"
-                x-text="status.records.reduce((acc, record) => acc + parseFloat(record[status.totalAttribute]), 0).toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' })">
+                x-text="(status.total !== undefined ? status.total : status.records.reduce((acc, record) => acc + parseFloat(record[status.totalAttribute]), 0)).toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' })">
             </span>
         </template>
     </div>
--- src/Pages/KanbanBoard.php
+++ src/Pages/KanbanBoard.php
@@ -38,6 +38,8 @@ class KanbanBoard extends Page
 
     public ?string $kanbanSearchValue = null;
 
+    protected int $perPage = 25;
+
     protected function statuses(): Collection
     {
         return static::$statusEnum::statuses();
